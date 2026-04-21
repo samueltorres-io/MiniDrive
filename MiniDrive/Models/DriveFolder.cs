7deab4343b02f1292d351123f1bf62211cbd8213
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-21 10:44:13 -0300

    Add DriveFolder model to represent folders in the MiniDrive application

--- MiniDrive/Models/DriveFolder.cs
+++ MiniDrive/Models/DriveFolder.cs
@@ -1,24 +1,24 @@
 namespace MiniDrive.Models;
 
-public class Folder
+public class DriveFolder
 {
     public int Id { get; set; }
 
     public int UserId { get; set; }
-    public User User { get; set; } = default!;
+    public DriveUser User { get; set; } = default!;
 
     public string Name { get; set; } = default!;
 
     public int? ParentId { get; set; }
-    public Folder? Parent { get; set; }
-    public ICollection<Folder> Children { get; set; } = new List<Folder>();
+    public DriveFolder? Parent { get; set; }
+    public ICollection<DriveFolder> Children { get; set; } = new List<DriveFolder>();
 
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
     public DateTime? DeletedAt { get; set; }
     public int? DeletedBy { get; set; }
-    public User? DeletedByUser { get; set; }
+    public DriveUser? DeletedByUser { get; set; }
 
-    public ICollection<File> Files { get; set; } = new List<File>();
+    public ICollection<DriveFile> Files { get; set; } = new List<DriveFile>();
 }
