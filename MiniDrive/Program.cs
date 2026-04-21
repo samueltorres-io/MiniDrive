1eea39de7b919cd37ee0ce02502927d21536527a
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-21 20:49:37 -0300

    Add database context configuration and register services in Program.cs for improved dependency injection

--- MiniDrive/Program.cs
+++ MiniDrive/Program.cs
@@ -1,17 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MiniDrive.Data;
+using MiniDrive.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddControllers();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
