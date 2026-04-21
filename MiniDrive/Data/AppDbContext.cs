349fc96037b8f18af3345e302952c2f999ac7b51
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-21 17:16:06 -0300

    Add folder entity configuration to AppDbContext in OnModelCreating method

--- MiniDrive/Data/AppDbContext.cs
+++ MiniDrive/Data/AppDbContext.cs
@@ -14,7 +14,7 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
-        /* User */
+        /* Users */
         modelBuilder.Entity<DriveUser>(e =>
         {
             e.ToTable("users");
@@ -23,6 +23,28 @@ public class AppDbContext : DbContext
                 .HasMaxLength(40)
                 .IsRequired();
             e.HasIndex(u => u.Username).IsUnique();
+        });
+
+        /* Folders */
+        modelBuilder.Entity<DriveFolder>(e =>
+        {
+            e.ToTable("folders");
+            e.HasKey(f => f.Id);
+            e.Property(f => f.Name).HasMaxLength(256).IsRequired();
+            e.HasIndex(f => f.UserId);
+            e.HasIndex(f => f.ParentId);
+            e.HasOne(f => f.User)
+                .WithMany(u => u.Folders)
+                .HasForeignKey(f => f.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+            e.HasOne(f => f.DeletedByUser)
+                .WithMany()
+                .HasForeignKey(f => f.DeletedBy)
+                .OnDelete(DeleteBehavior.Restrict);
+            e.HasOne(f => f.Parent)
+                .WithMany(f => f.Children)
+                .HasForeignKey(f => f.ParentId)
+                .OnDelete(DeleteBehavior.Restrict);
         });
 
     }
