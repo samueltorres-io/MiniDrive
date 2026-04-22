caa1553866fe6d96c31789104ae4f3991046f507
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-22 18:11:13 -0300

    Add Minio configuration and CORS support in Program.cs
    - Integrate Minio service for object storage with configuration from app settings.
    - Implement CORS policy to allow requests from localhost:4200, enabling frontend communication.
    - Ensure SSL is disabled for Minio connections.

--- MiniDrive/Program.cs
+++ MiniDrive/Program.cs
@@ -8,6 +8,15 @@ builder.Services.AddDbContext<AppDbContext>(opts =>
     opts.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
         .UseSnakeCaseNamingConvention());
 
+builder.Services.AddMinio(opts =>
+{
+    opts.Endpoint  = builder.Configuration["Minio__Endpoint"]!
+                             .Replace("http://", "").Replace("https://", "");
+    opts.AccessKey = builder.Configuration["Minio__AccessKey"]!;
+    opts.SecretKey = builder.Configuration["Minio__SecretKey"]!;
+    opts.WithSSL(false);
+});
+
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IFolderService, FolderService>();
 builder.Services.AddScoped<IFileService, FileService>();
@@ -15,6 +24,16 @@ builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -28,6 +47,8 @@ if (app.Environment.IsDevelopment())
 
     app.MapOpenApi();
 }
+
+app.UseCors();
 
 app.MapControllers();
 
